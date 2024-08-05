3aa060619d77c500bd7493deac40500025390b05
shadrach77 <84427037+shadrach77@users.noreply.github.com>
2024-08-05 17:04:57 +0700

    tm added

--- src/app/(main)/about-us/page.tsx
+++ src/app/(main)/about-us/page.tsx
@@ -5,19 +5,19 @@ function AboutUs() {
     <div className="h-auto min-h-screen w-screen bg-[#f3b700] flex justify-center items-center flex-col">
       <h1 className="text-6xl text-[#dd6e42] px-16 py-8 font-bold">About Us</h1>
       <p className="text-2xl text-[#fbf7ec] px-16 pb-16">
-        At Virtual Escapes<span>&#8482;</span>, we bring the world's wonders to
-        your fingertips. Through our cutting-edge VR technology, you can
-        experience breathtaking landscapes, vibrant cities, and serene retreats
-        without leaving home. Whether you're craving a peaceful beach, a
-        mountain adventure, or a cultural exploration, our immersive virtual
-        vacations transport you to extraordinary destinations. Founded during
-        the challenging times of COVID-19, we strive to make exploration
-        accessible to everyone, everywhere. Our expert team crafts each
-        experience to be visually stunning and emotionally engaging, providing a
-        true sense of escape and adventure. Discover the joy of travel from your
-        own private space. With Virtual Escapes, your next adventure is just a
-        strap away. Embrace the future of travel and explore the extraordinary.
-        Welcome to Virtual Escapes<span>&#8482;</span>.
+        At Virtual Escapes<span>™</span>, we bring the world's wonders to your
+        fingertips. Through our cutting-edge VR technology, you can experience
+        breathtaking landscapes, vibrant cities, and serene retreats without
+        leaving home. Whether you're craving a peaceful beach, a mountain
+        adventure, or a cultural exploration, our immersive virtual vacations
+        transport you to extraordinary destinations. Founded during the
+        challenging times of COVID-19, we strive to make exploration accessible
+        to everyone, everywhere. Our expert team crafts each experience to be
+        visually stunning and emotionally engaging, providing a true sense of
+        escape and adventure. Discover the joy of travel from your own private
+        space. With Virtual Escapes, your next adventure is just a strap away.
+        Embrace the future of travel and explore the extraordinary. Welcome to
+        Virtual Escapes<span>™</span>.
       </p>
     </div>
   );
